8e7912ee6c1d06819935f5d98f2a2b3baa8e6c8e
viet.le <viet.le@sonicviet.com>
2013-07-31 23:30:57 +0700

    - Add junit for test  On branch master 	modified:   RecognitionFingerprint/.classpath 	deleted:    RecognitionFingerprint/bin/com/vnexit/fingerprint/form/1_1.bmp 	deleted:    RecognitionFingerprint/bin/com/vnexit/fingerprint/form/vantay1/16_1.png 	deleted:    RecognitionFingerprint/bin/com/vnexit/fingerprint/form/vantay1/16_2.png 	deleted:    RecognitionFingerprint/bin/com/vnexit/fingerprint/form/vantay1/16_3.png 	deleted:    RecognitionFingerprint/bin/com/vnexit/fingerprint/form/vantay1/16_4.png 	deleted:    RecognitionFingerprint/bin/com/vnexit/fingerprint/form/vantay1/16_5.png 	deleted:    RecognitionFingerprint/bin/com/vnexit/fingerprint/form/vantay1/16_6.png 	deleted:    RecognitionFingerprint/bin/com/vnexit/fingerprint/form/vantay1/16_7.png 	deleted:    RecognitionFingerprint/bin/com/vnexit/fingerprint/form/vantay1/16_8.png 	deleted:    RecognitionFingerprint/bin/com/vnexit/fingerprint/form/vantay1/vantay2.png 	new file:   RecognitionFingerprint/test/src/com/vnexit/fingerprint/datamodel/TestImageProcess.java

---
+++ RecognitionFingerprint/test/src/com/vnexit/fingerprint/datamodel/TestImageProcess.java
@@ -0,0 +1,36 @@
+package com.vnexit.fingerprint.datamodel;
+
+import static org.junit.Assert.*;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestImageProcess {
+	BufferedImage mFinger;
+	Pixel[][] pi;
+	ImageProcess imgProcess;
+	WritableRaster wr;
+
+	@Test
+	public void testConvertImagetoGray() throws IOException {
+		mFinger = ImageIO.read(getClass().getResource(
+				"../form/vantay1/vantay2.png"));
+		imgProcess = new ImageProcess(mFinger);
+		wr = mFinger.getRaster();
+		pi = imgProcess.grayExchange();
+		Assert.assertEquals(pi.length, mFinger.getHeight());
+		for (int i = 0; i < mFinger.getWidth(); i++) {
+			for (int j = 0; j < mFinger.getHeight(); j++) {
+				Assert.assertEquals(pi[i][j].mBlue, pi[i][j].mGreen);
+				System.out.println(pi[i][j].mBlue);
+			}
+		}
+	}
+
+}
