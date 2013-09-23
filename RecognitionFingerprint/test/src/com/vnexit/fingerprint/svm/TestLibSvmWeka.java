b5b3934d8feda96902dedc9a343ad1a62c5a9e54
viet.le <viet.le@sonicviet.com>
2013-09-23 21:08:56 +0700

    Add libsvm  On branch ExtractFeatured 	modified:   RecognitionFingerprint/.classpath 	new file:   RecognitionFingerprint/test/src/com/vnexit/fingerprint/datatest/iris.arff 	new file:   RecognitionFingerprint/test/src/com/vnexit/fingerprint/svm/TestLibSvmWeka.java

---
+++ RecognitionFingerprint/test/src/com/vnexit/fingerprint/svm/TestLibSvmWeka.java
@@ -0,0 +1,26 @@
+package com.vnexit.fingerprint.svm;
+
+
+public class TestLibSvmWeka {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// try {
+		// FileReader fr1 = new FileReader("../datatest/iris.arff");
+		// Instances one = new Instances(fr1);
+		// one.setClassIndex(1);
+		System.out.println(Math.sin('q' * 5 * 11));
+
+		// } catch (FileNotFoundException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// } catch (IOException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+
+	}
+
+}
