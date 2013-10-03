4249e056821c85cbdb53084065a10770c82d1540
viet.le <viet.le@sonicviet.com>
2013-10-03 23:13:39 +0700

    On branch AddLibSVM 	modified:   RecognitionFingerprint/src/com/vnexit/fingerprint/datamodel/ExtractMinutiae.java

--- RecognitionFingerprint/src/com/vnexit/fingerprint/datamodel/ExtractMinutiae.java
+++ RecognitionFingerprint/src/com/vnexit/fingerprint/datamodel/ExtractMinutiae.java
@@ -1,7 +1,7 @@
 package com.vnexit.fingerprint.datamodel;
 
 public class ExtractMinutiae {
-	private final Pixel[][] mPi;
+	private static int mNumber = 20;
 	private final double[][] mTeta;
 	private final double[][] mFeatured;
 
@@ -10,11 +10,36 @@ public class ExtractMinutiae {
 	 * @param mTeta
 	 * @param mFeatured
 	 */
-	public ExtractMinutiae(Pixel[][] mPi, double[][] mTeta, double[][] mFeatured) {
+	public ExtractMinutiae(double[][] mTeta, double[][] mFeatured) {
 		super();
-		this.mPi = mPi;
 		this.mTeta = mTeta;
 		this.mFeatured = mFeatured;
 	}
 
+	private double[] convertArray(double[][] array) {
+		double[] res = new double[array.length * array[0].length];
+		int k = 0;
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[0].length; j++) {
+				res[k] = array[i][j];
+				k++;
+			}
+		}
+		return res;
+	}
+
+	private void sortArray(double[] array, int[] tick) {
+		for (int i = 0; i < array.length; i++) {
+			for (int j = i + 1; j < array.length; j++) {
+				if (array[i] > array[j]) {
+					double tmp = array[i];
+					array[i] = array[j];
+					array[j] = tmp;
+					int ti = tick[i];
+					tick[i] = tick[j];
+					tick[j] = ti;
+				}
+			}
+		}
+	}
 }
